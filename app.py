8259b85e277a2a20aeb26965a7b408a70422e54f
xuegongao <gongao_xue@berkeley.edu>
2023-04-29 20:37:18 -0700

    sort by name

--- app.py
+++ app.py
@@ -2,5 +2,4 @@ from config import app
 import os
 
 if __name__ == "__main__":
-    # app.run(host=os.environ["HOST"], port=int(os.environ["PORT"]), debug=True)
-    app.run(host=os.environ["HOST"], port=3000, debug=True)
+    app.run(host=os.environ["HOST"], port=int(os.environ["PORT"]), debug=True)
