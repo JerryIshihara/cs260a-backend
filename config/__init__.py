d42a15c176e26b139d6e516f6d3e4db311bb00d0
JerryIshihara <hirotaka.ishihara@mail.utoronto.ca>
2023-05-02 21:05:40 -0700

    init database

--- config/__init__.py
+++ config/__init__.py
@@ -15,14 +15,28 @@ load_dotenv(dotenv_path)
 # init app instance
 app = Flask("cs260a")
 
+import subprocess
+if not os.path.exists(os.path.join(basedir, "database/database.db")):
+    command = "mkdir database && touch database/database.db"
+    result = subprocess.run(command,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            text=True,
+                            shell=True)
 
+# Print the output of the command
+print("Output:", result.stdout)
+
+# Print any errors that occurred
+if result.stderr:
+    print("Error:", result.stderr)
 
 # settup sql-alchemy config
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database/database.db")
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir,
+                                                      "database/database.db")
 print(dotenv_path)
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 
 # init CORS
 cors = CORS(app,
@@ -35,7 +49,6 @@ cors = CORS(app,
                 "Access-Control-Allow-Credentials",
             ],
             supports_credentials=True)
-
 
 # init database and schema
 db = SQLAlchemy(app)
