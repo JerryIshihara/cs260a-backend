0985499c5044577cfdd9b5fd2d83a7575e779b8f
Lei Chen <leichen753@gmail.com>
2023-05-02 21:47:09 -0700

    avatar url

--- model/videoComments/__init__.py
+++ model/videoComments/__init__.py
@@ -8,7 +8,7 @@ def get_current_time():
     return str(datetime.datetime.now(pytz.timezone('US/Pacific')))
 
 def parse_comment(comment):
-    return {'comment_key': comment.comment_key, 'aws_key': comment.aws_key, 'user_id': comment.user_id, 'comment': comment.comment, 'created_at': comment.created_at}
+    return {'comment_key': comment.comment_key, 'aws_key': comment.aws_key, 'user_id': comment.user_id, 'comment': comment.comment, 'created_at': comment.created_at, 'avatar_url': comment.avatar_url}
 
 class VideoComments(db.Model):
     __tablename__ = "VideoComments"
@@ -17,6 +17,7 @@ class VideoComments(db.Model):
     user_id = db.Column(db.String, nullable=False)
     comment = db.Column(db.String, nullable=False)
     created_at = db.Column(db.String, nullable=False)
+    avatar_url = db.Column(db.String, nullable=False)
 
     # def __init__(self, data):
     #     for key in data.keys():
@@ -24,9 +25,9 @@ class VideoComments(db.Model):
 
 
     @staticmethod
-    def insert_video_comment(aws_key, user_id, comment):
+    def insert_video_comment(aws_key, user_id, comment, avatar_url):
         cur_time = get_current_time()
-        new_comment = VideoComments(aws_key = aws_key, user_id = user_id, comment = comment, created_at = cur_time)
+        new_comment = VideoComments(aws_key = aws_key, user_id = user_id, comment = comment, created_at = cur_time, avatar_url = avatar_url)
         db.session.add(new_comment)
         db.session.commit()
 
